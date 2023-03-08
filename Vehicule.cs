a3f332ca590953ded80f82968926c92e20c22925
RBREHAM <raphael.breham@viacesi.fr>
2023-03-08 13:48:20 +0100

    feat(animals) : Finish

---
+++ Vehicule.cs
@@ -0,0 +1,25 @@
+﻿namespace CSharpAnimal
+{
+    class Vehicule
+    {
+        public Vehicule(string name)
+        {
+            this.name = name;
+        }
+
+        public string name { get; set; }
+
+        public virtual void embarquer()
+        {
+            Console.WriteLine("On embarque !");
+        }
+        public virtual void avancer()
+        {
+            Console.WriteLine("On avance !");
+        }
+        public virtual void reculer()
+        {
+            Console.WriteLine("On recule !");
+        }
+    }
+}
